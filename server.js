ee5c7f16fb46822a82852c0a9b8ca727d7ee4b6e
Corey Shott <corey.shott@adroll.com>
2017-02-05 18:46:24 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
